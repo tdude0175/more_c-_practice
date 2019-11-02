c4aca607e08c59791fd8a4aae0fa39a961ebb90d
tdude0175 <tdude0175@gmail.com>
2019-11-02 14:15:37 -0500

    initial commit adding gitignore for the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace more_c__practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
